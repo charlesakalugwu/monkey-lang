2f5b5af5acf186984192243c4e65885401917325
charlesakalugwu <akalugwu@redhat.com>
2019-07-04 01:23:03 +0200

    Add initial token definitions

--- token/token.go
+++ token/token.go
@@ -1,1 +1,35 @@
 package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	// Grouping
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Type string
+
+type Token struct {
+	Type    Type
+	Literal string
+}
